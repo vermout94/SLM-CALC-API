091212b5f0e95a2f362c84c0dac387cf44535a70
vermout94 <david.berger4494@gmail.com>
2023-06-06 18:45:45 +0200

    initial commit

---
+++ src/main/java/at/technikum/slmcalc/SlmCalcApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.slmcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/slmcalc/SlmCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
